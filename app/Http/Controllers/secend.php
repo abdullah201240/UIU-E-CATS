c94416bdbe0227c5c0e0d10943a3bfaddba05ee6
Shabrina Airen Esha <eshaairen851@gmail.com>
2023-04-25 01:15:26 +0600

    hostel

--- app/Http/Controllers/secend.php
+++ app/Http/Controllers/secend.php
@@ -15,23 +15,25 @@ use Illuminate\Support\Facades\Storage;
 
 class secend extends Controller
 {
-    public function deleteua($id){
+    public function deleteua($id)
+    {
 
         DB::delete('DELETE FROM `apply` WHERE id= ?', [$id]);
 
         return redirect("uahome");
     }
-    public function deletegrader($id){
+    public function deletegrader($id)
+    {
 
         DB::delete('DELETE FROM `apply` WHERE id= ?', [$id]);
 
         return redirect("graderhome");
     }
-    public function hostel_registration_form(Request $req){
+    public function hostel_registration_form(Request $req)
+    {
         $data = array('sid' => $req->id, "email" => $req->email, "sname" => $req->name, "phone" => $req->phoneNumber, "gender" => $req->gender, "status" => "Panding");
         DB::table('hostel')->insert($data);
 
-return redirect("hostel_registration_form");
-
+        return redirect("hostel_registration_form");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,9 @@ Route::get('/', function () {
 
 Route::get('home', [student_course_enroll::class, 'index']);
 
-Route::get('addcourses',[student_course_enroll::class, 'addcourse']);
-Route::get('studentdeletecourse/{id}',[student_course_enroll::class, 'destroy']);
-Route::get('counsilling',[student_course_enroll::class, 'studentinfo']);
+Route::get('addcourses', [student_course_enroll::class, 'addcourse']);
+Route::get('studentdeletecourse/{id}', [student_course_enroll::class, 'destroy']);
+Route::get('counsilling', [student_course_enroll::class, 'studentinfo']);
 
 Route::get("slogin", [student_course_enroll::class, 'login']);
 Route::view("login", "slogin");
@@ -36,7 +36,7 @@ Route::get("searchcourses", [student_course_enroll::class, 'searchcourses']);
 
 Route::get("addcoursesapi/{id}", [student_course_enroll::class, 'addcourses']);
 Route::get('slogout', [student_course_enroll::class, 'slogout']);
-Route::get('deletecounsling/{id}',[student_course_enroll::class, 'destroys']);
+Route::get('deletecounsling/{id}', [student_course_enroll::class, 'destroys']);
 
 Route::get('studentaddcounsilling', [student_course_enroll::class, 'showallteacher']);
 
